249401c18913ac70e0ae23a3d178b3998eb75dac
GoldenPig1205 <78902671+GoldenPig1205@users.noreply.github.com>
2024-07-06 17:23:23 +0900

    079 공허 버그 수정 & 고문 추가

---
+++ GPOffice/Modes/Cell.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.API.Features;
+using MEC;
+using Mirror;
+using UnityEngine;
+
+namespace GPOffice.Modes
+{
+    class Cell
+    {
+        public static Cell Instance;
+
+        public List<Player> pl = new List<Player>();
+
+        public void OnEnabled()
+        {
+            Timing.RunCoroutine(OnModeStarted());
+
+            Exiled.Events.Handlers.Player.Died += OnDied;
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            Server.ExecuteCommand("/mp load cell");
+            Player BadLucky = GPOffice.GetRandomValue(Player.List.ToList());
+
+            yield return Timing.WaitForSeconds(1f);
+
+            foreach (var player in Player.List)
+            {
+                player.Role.Set(PlayerRoles.RoleTypeId.Tutorial);
+                player.Position = new Vector3(118.7332f, 1000.379f, -41.59417f);
+
+                if (player == BadLucky)
+                    Server.ExecuteCommand($"/drop {player.Id} 31");
+            }
+        }
+
+        public void OnDied(Exiled.Events.EventArgs.Player.DiedEventArgs ev)
+        {
+            if (pl.Contains(ev.Player))
+            {
+                pl.Remove(ev.Player);
+
+                if (pl.Count < 2)
+                    Round.IsLocked = false;
+            }
+        }
+    }
+}
