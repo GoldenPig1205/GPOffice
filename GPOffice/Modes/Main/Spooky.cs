79f9343d9516c383f0e0c62849ba66d6f672b527
GoldenPig1205 <78902671+GoldenPig1205@users.noreply.github.com>
2024-08-19 17:13:39 +0900

    update

---
+++ GPOffice/Modes/Main/Spooky.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomRendering;
+using Exiled.API.Features;
+using MEC;
+using Mirror;
+using UnityEngine;
+
+namespace GPOffice.Modes
+{
+    class Spooky
+    {
+        public static Spooky Instance;
+
+        public void OnEnabled()
+        {
+            Timing.RunCoroutine(OnModeStarted());
+        }
+
+        public IEnumerator<float> OnModeStarted()
+        {
+            Map.ChangeLightsColor(Color.HSVToRGB(0.01f, 0.01f, 0.01f));
+        }
+    }
+}
