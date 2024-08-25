fdfbbc48195fa7bf64c018d12535f398c7acb7d0
박경현 <78902671+GoldenPig1205@users.noreply.github.com>
2024-08-26 08:48:40 +0900

    서브 모드ㅣ[역전 세계] 추가

---
+++ GPOffice/Modes/Sub/ReversedHuman.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomRendering;
+using Exiled.API.Features;
+using MEC;
+using Mirror;
+using UnityEngine;
+
+namespace GPOffice.SubModes
+{
+    class ReversedHuman
+    {
+        public static ReversedHuman Instance;
+
+        public List<Player> pl = new List<Player>();
+
+        public void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Spawned += OnSpawned;
+        }
+
+        public void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Spawned -= OnSpawned;
+        }
+
+        public void OnSpawned(Exiled.Events.EventArgs.Player.SpawnedEventArgs ev)
+        {
+            ev.Player.Scale = new Vector3(-1f, -1f, -1f);
+        }
+    }
+}
